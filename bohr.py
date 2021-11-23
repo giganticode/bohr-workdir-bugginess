e2f1d8b40ab21ccd850da8b3583e7d5a68bb168c
hlib <hlibbabii@gmail.com>
2021-11-23 09:06:21 +0100

    initial commit with latest version of heurstics

---
+++ bohr.py
@@ -0,0 +1,29 @@
+from bohrapi.artifacts import Commit
+from bohrlabels.labels import CommitLabel
+from bohrapi.core import Dataset, Task, Workspace
+
+
+HEURISTICS_CLASSIFIER = '.' # run all heuristics
+BOHR_FRAMEWORK_VERSION = '0.5.0rc0'
+
+commits_200k = Dataset(id='bohr.200k_commits', top_artifact=Commit,)
+
+berger = Dataset(id='manual_labels.berger',
+                 top_artifact=Commit,
+                 get_bohr_label_for_datapoint=lambda c: (CommitLabel.BugFix if c.raw_data['manual_labels']['berger']['bug'] == 1 else CommitLabel.NonBugFix))
+
+levin = Dataset(id='manual_labels.levin',
+                 top_artifact=Commit,
+                 get_bohr_label_for_datapoint=lambda c: (CommitLabel.BugFix if c.raw_data['manual_labels']['levin']['bug'] == 1 else CommitLabel.NonBugFix))
+
+herzig = Dataset(id='manual_labels.herzig',
+                 top_artifact=Commit,
+                 get_bohr_label_for_datapoint=lambda c: (CommitLabel.BugFix if c.raw_data['manual_labels']['herzig']['bug'] == 1 else CommitLabel.NonBugFix))
+
+bugginess = Task(name='bugginess', author='hlib', description='bug or not', top_artifact=Commit,
+                 labels=[CommitLabel.NonBugFix, CommitLabel.BugFix],
+                 training_dataset=commits_200k,
+                 test_datasets=[levin, berger, herzig], heuristics_classifier=f'{HEURISTICS_CLASSIFIER}@bohr-0.5')
+
+w = Workspace(BOHR_FRAMEWORK_VERSION, [bugginess])
+
