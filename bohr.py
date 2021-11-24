fd23e0940151fdf96bb0bf0f9d66243b32fa213f
hlib <hlibbabii@gmail.com>
2021-11-24 13:38:13 +0100

    only clean message keywords

--- bohr.py
+++ bohr.py
@@ -23,7 +23,7 @@ herzig = Dataset(id='manual_labels.herzig',
 bugginess = Task(name='bugginess', author='hlib', description='bug or not', top_artifact=Commit,
                  labels=[CommitLabel.NonBugFix, CommitLabel.BugFix],
                  training_dataset=commits_200k,
-                 test_datasets=[levin, berger, herzig], heuristics_classifier=f'{HEURISTICS_CLASSIFIER}@988e934bcd9447d18ddf4af8957ceef286c8d2d7')
+                 test_datasets=[levin, berger, herzig], heuristics_classifier=f'{HEURISTICS_CLASSIFIER}@29dfeed9fe2a9f43b6dd8583b141696c4db178d5')
 
 w = Workspace(BOHR_FRAMEWORK_VERSION, [bugginess])
 
