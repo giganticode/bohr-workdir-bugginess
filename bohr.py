49594d1c7a6cefc72e2fe598021bbdc7e2a3d30a
hlib <hlibbabii@gmail.com>
2022-07-15 08:22:04 +0200

    update config according to the new api

--- bohr.py
+++ bohr.py
@@ -3,8 +3,9 @@ from typing import Dict, Optional, List, Any
 from pydot import frozendict
 
 from bohrapi.artifacts import Commit
+from bohrlabels.core import LabelSet
 from bohrlabels.labels import CommitLabel
-from bohrapi.core import Dataset, Task, Workspace, Experiment
+from bohrapi.core import Dataset, Workspace, Experiment, LabelingTask
 
 
 def id_with_files(id: str, conditions: Optional[List[Dict]] = None) -> Dict:
@@ -33,40 +34,40 @@ def important_project_query(datasets: List[str]) -> Dict[str, Any]:
     return query
 
 
-commits_200k = Dataset(id='commits_200k', top_artifact=Commit, query={'bohr.200k_commits': {"$exists": True}})
-commits_200k_files = Dataset(id='commits_200k_files', top_artifact=Commit, query=id_with_files('bohr.200k_commits'))
-commits_200k_files_0 = Dataset(id='commits_200k_files_0', top_artifact=Commit, query=id_with_files('bohr.200k_commits', [{"_id": {'$regex':'^0'}}]))
-commits_200k_files_00 = Dataset(id='commits_200k_files_00', top_artifact=Commit, query=id_with_files('bohr.200k_commits', [{"_id": {'$regex':'^00'}}]))
-commits_200k_files_000 = Dataset(id='commits_200k_files_000', top_artifact=Commit, query=id_with_files('bohr.200k_commits', [{"_id": {'$regex':'^000'}}]))
-commits_200k_files_no_merges = Dataset(id='commits_200k_files_no_merges', top_artifact=Commit, query=id_with_files("bohr.200k_commits", [{"special_commit_finder/0_1.merge": False}]))
-levin_berger_herzig_train = Dataset(id='levin_berger_herzig_train', top_artifact=Commit, query=important_project_query(['levin', 'berger', 'herzig']), projection={'gumtree/3_0_0-beta2': 0}, n_datapoints=20000)
-levin_train = Dataset(id='levin_train', top_artifact=Commit, query=important_project_query(['levin']), projection={'gumtree/3_0_0-beta2': 0}, n_datapoints=20000)
-berger_train = Dataset(id='berger_train', top_artifact=Commit, query=important_project_query(['berger']), projection={'gumtree/3_0_0-beta2': 0}, n_datapoints=20000)
-herzig_train = Dataset(id='herzig_train', top_artifact=Commit, query=important_project_query(['herzig']), projection={'gumtree/3_0_0-beta2': 0}, n_datapoints=20000)
+commits_200k = Dataset(id='commits_200k', heuristic_input_artifact_type=Commit, query={'bohr.200k_commits': {"$exists": True}})
+commits_200k_files = Dataset(id='commits_200k_files', heuristic_input_artifact_type=Commit, query=id_with_files('bohr.200k_commits'))
+commits_200k_files_0 = Dataset(id='commits_200k_files_0', heuristic_input_artifact_type=Commit, query=id_with_files('bohr.200k_commits', [{"_id": {'$regex':'^0'}}]))
+commits_200k_files_00 = Dataset(id='commits_200k_files_00', heuristic_input_artifact_type=Commit, query=id_with_files('bohr.200k_commits', [{"_id": {'$regex':'^00'}}]))
+commits_200k_files_000 = Dataset(id='commits_200k_files_000', heuristic_input_artifact_type=Commit, query=id_with_files('bohr.200k_commits', [{"_id": {'$regex':'^000'}}]))
+commits_200k_files_no_merges = Dataset(id='commits_200k_files_no_merges', heuristic_input_artifact_type=Commit, query=id_with_files("bohr.200k_commits", [{"special_commit_finder/0_1.merge": False}]))
+levin_berger_herzig_train = Dataset(id='levin_berger_herzig_train', heuristic_input_artifact_type=Commit, query=important_project_query(['levin', 'berger', 'herzig']), projection={'gumtree/3_0_0-beta2': 0}, n_datapoints=20000)
+levin_train = Dataset(id='levin_train', heuristic_input_artifact_type=Commit, query=important_project_query(['levin']), projection={'gumtree/3_0_0-beta2': 0}, n_datapoints=20000)
+berger_train = Dataset(id='berger_train', heuristic_input_artifact_type=Commit, query=important_project_query(['berger']), projection={'gumtree/3_0_0-beta2': 0}, n_datapoints=20000)
+herzig_train = Dataset(id='herzig_train', heuristic_input_artifact_type=Commit, query=important_project_query(['herzig']), projection={'gumtree/3_0_0-beta2': 0}, n_datapoints=20000)
 
-berger_files = Dataset(id='berger_files', top_artifact=Commit, query=id_with_files('manual_labels.berger'))
-levin_files = Dataset(id='levin_files', top_artifact=Commit, query=id_with_files('manual_labels.levin'))
-herzig = Dataset(id='manual_labels.herzig', top_artifact=Commit)
-mauczka_files = Dataset(id='mauczka_files', top_artifact=Commit, query=id_with_files('manual_labels.mauczka'))
-idan_files = Dataset(id='idan_files', top_artifact=Commit, query=id_with_files('idan/0_1', [{"idan/0_1.Is_Corrective": {"$exists": True}}]))
+berger_files = Dataset(id='berger_files', heuristic_input_artifact_type=Commit, query=id_with_files('manual_labels.berger'))
+levin_files = Dataset(id='levin_files', heuristic_input_artifact_type=Commit, query=id_with_files('manual_labels.levin'))
+herzig = Dataset(id='manual_labels.herzig', heuristic_input_artifact_type=Commit)
+mauczka_files = Dataset(id='mauczka_files', heuristic_input_artifact_type=Commit, query=id_with_files('manual_labels.mauczka'))
+idan_files = Dataset(id='idan_files', heuristic_input_artifact_type=Commit, query=id_with_files('idan/0_1', [{"idan/0_1.Is_Corrective": {"$exists": True}}]))
 
-bohr_herzig_train = Dataset(id='bohr.herzig_train', top_artifact=Commit)
-bohr_herzig_eval = Dataset(id='bohr.herzig_eval', top_artifact=Commit)
+bohr_herzig_train = Dataset(id='bohr.herzig_train', heuristic_input_artifact_type=Commit)
+bohr_herzig_eval = Dataset(id='bohr.herzig_eval', heuristic_input_artifact_type=Commit)
 
-levin_small_changes = Dataset(id='levin_small_changes', top_artifact=Commit, query=id_with_files('manual_labels.levin', [{'bohr.gt_512_codeberta_tokens': False}]))
-levin_large_changes = Dataset(id='levin_large_changes', top_artifact=Commit, query=id_with_files('manual_labels.levin', [{'bohr.gt_512_codeberta_tokens': True}]))
+levin_small_changes = Dataset(id='levin_small_changes', heuristic_input_artifact_type=Commit, query=id_with_files('manual_labels.levin', [{'bohr.gt_512_codeberta_tokens': False}]))
+levin_large_changes = Dataset(id='levin_large_changes', heuristic_input_artifact_type=Commit, query=id_with_files('manual_labels.levin', [{'bohr.gt_512_codeberta_tokens': True}]))
 
-berger_small_changes = Dataset(id='berger_small_changes', top_artifact=Commit, query=id_with_files('manual_labels.berger', [{'bohr.gt_512_codeberta_tokens': False}]))
-berger_large_changes = Dataset(id='berger_large_changes', top_artifact=Commit, query=id_with_files('manual_labels.berger', [{'bohr.gt_512_codeberta_tokens': True}]))
+berger_small_changes = Dataset(id='berger_small_changes', heuristic_input_artifact_type=Commit, query=id_with_files('manual_labels.berger', [{'bohr.gt_512_codeberta_tokens': False}]))
+berger_large_changes = Dataset(id='berger_large_changes', heuristic_input_artifact_type=Commit, query=id_with_files('manual_labels.berger', [{'bohr.gt_512_codeberta_tokens': True}]))
 
-bohr_200k_small_changes = Dataset(id='bohr_200k_small_changes', top_artifact=Commit, query=id_with_files('bohr.200k_commits', [{'bohr.gt_512_codeberta_tokens': False}]))
-bohr_200k_large_changes = Dataset(id='bohr_200k_large_changes', top_artifact=Commit, query=id_with_files('bohr.200k_commits', [{'bohr.gt_512_codeberta_tokens': True}]))
+bohr_200k_small_changes = Dataset(id='bohr_200k_small_changes', heuristic_input_artifact_type=Commit, query=id_with_files('bohr.200k_commits', [{'bohr.gt_512_codeberta_tokens': False}]))
+bohr_200k_large_changes = Dataset(id='bohr_200k_large_changes', heuristic_input_artifact_type=Commit, query=id_with_files('bohr.200k_commits', [{'bohr.gt_512_codeberta_tokens': True}]))
 
 #conventional_with_files = Dataset(id='conventional_with_files', top_artifact=Commit, query={'conventional_commit/0_1.conventional': True, 'message': {"$exists": True}, 'files': {"$exists": True}}, n_datapoints=20000)
 
 
-bugginess = Task(name='bugginess', author='hlib', description='bug or not', top_artifact=Commit,
-                 labels=[CommitLabel.NonBugFix, CommitLabel.BugFix],
+bugginess = LabelingTask(name='bugginess', author='hlib', description='bug or not', heuristic_input_artifact_type=Commit,
+                 labels=(CommitLabel.NonBugFix, CommitLabel.BugFix),
                  test_datasets={
                                 idan_files: lambda c: (CommitLabel.BugFix if c.raw_data['idan/0_1']['Is_Corrective'] else CommitLabel.NonBugFix),
                                 levin_files: lambda c: (CommitLabel.BugFix if c.raw_data['manual_labels']['levin']['bug'] == 1 else CommitLabel.NonBugFix),
@@ -75,13 +76,16 @@ bugginess = Task(name='bugginess', author='hlib', description='bug or not', top_
                                 mauczka_files: lambda c: (CommitLabel.BugFix if c.raw_data['manual_labels']['mauczka']['hl_corrective'] == 1 else CommitLabel.NonBugFix),
                                 })
 
-refactoring = Task(name='refactoring', author='hlib', description='refactoring or not', top_artifact=Commit,
-                 labels=[CommitLabel.CommitLabel & ~CommitLabel.Refactoring, CommitLabel.Refactoring],
-                 test_datasets={
-                     herzig: lambda c: (CommitLabel.Refactoring if c.raw_data['manual_labels']['herzig']['CLASSIFIED'] == 'REFACTORING' else CommitLabel.CommitLabel & ~CommitLabel.Refactoring),
-                 })
+refactoring = LabelingTask(name='refactoring',
+                           author='hlib',
+                           description='refactoring or not',
+                           heuristic_input_artifact_type=Commit,
+                           labels=(~LabelSet.of(CommitLabel.Refactoring), LabelSet.of(CommitLabel.Refactoring)),
+                           test_datasets={
+                               herzig: lambda c: (CommitLabel.Refactoring if c.raw_data['manual_labels']['herzig']['CLASSIFIED'] == 'REFACTORING' else CommitLabel.CommitLabel & ~CommitLabel.Refactoring),
+                           })
 
-REVISION = '0a56f59c95ca87ee324c365bf2eb71b212a6045e'
+REVISION = '5e101b0d8dc87680d6f6b7af012feffbc55834be'
 
 refactoring_no_ref_heuristics = Experiment('refactoring_no_ref_heuristics', refactoring, train_dataset=herzig_train,
                                       heuristics_classifier=f'bugginess/keywords/bug_keywords_lookup_in_message.py:'
